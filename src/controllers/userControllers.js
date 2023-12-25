239f4cd9b5107cc35d743320a18dbdf593d23113
nahjd <nahidmemmedov711@gmail.com>
2023-12-25 09:28:58 +0400

    asdasd

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,38 +1,47 @@
-const User = require("./../models/userModel")
+const User = require("./../models/userModel");
 
 const getAllUsers = async (req, res) => {
-    const allUsers = await User.find({})
-    res.send(allUsers)
-}
+  const allUsers = await User.find({});
+  res.send(allUsers);
+};
 
 const postUser = async (req, res) => {
-    let found = await User.findOne({ id: req.body.id })
-    if (found) {
-        console.log("This id exist")
-    } else {
-        const newUser = new User(req.body)
-        newUser.save()
-    }
-}
-
+  let found = await User.findOne({ _id: req.body.id });
+  if (found) {
+    console.log("This id exist");
+  } else {
+    const newUser = new User(req.body);
+    newUser.save();
+  }
+};
 
 const getUserById = async (req, res) => {
-    let found = await User.findOne({ id: req.params.id })
-    res.send(found)
-}
+  let found = await User.findOne({ _id: req.params.id });
+  res.send(found);
+};
 
 const deleteUser = async (req, res) => {
-    let found = await User.findOne({ id: req.params.id })
-    let _id = found._id
-    const deletedUser = await User.findByIdAndDelete(_id)
-}
+  let found = await User.findOne({ _id: req.params.id });
+  let _id = found._id;
+  const deletedUser = await User.findByIdAndDelete(_id);
+};
 
 const updateUser = async (req, res) => {
-    let updatedUser = await User.findOneAndUpdate({ id: req.params.id }, req.body)
-}
+  let updatedUser = await User.findOneAndUpdate(
+    { _id: req.params.id },
+    req.body
+  );
+};
 
 const putUser = async (req, res) => {
-    let updatedUser = await User.replaceOne({ id: req.params.id }, req.body)
-}
+  let updatedUser = await User.replaceOne({ _id: req.params.id }, req.body);
+};
 
-module.exports = { getAllUsers, getUserById, postUser, deleteUser, updateUser, putUser }
+module.exports = {
+  getAllUsers,
+  getUserById,
+  postUser,
+  deleteUser,
+  updateUser,
+  putUser,
+};
